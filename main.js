94d7cdf24e4421dfc134e943c26b1b2c9256df05
Ahsan Tahir <147637503+ahsantahirr@users.noreply.github.com>
2024-04-18 12:12:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+
+// Q:1 write a program to calculate the length of a given string?
+
+// let userName = "Ahsan Tahir";
+// console.log(userName.length);
+
+
+// Q:2 Convert a string to upper case?
+
+// let userName = "Ahsan Tahir"
+// let uppercase= userName.toUpperCase();
+// console.log(uppercase)
+
+
+// Q:3 Convert a string to lower case?
+
+//  let userName = "Ahsan Tahir"
+//  let lowercase= userName.toLowerCase();
+//  console.log(lowercase)
+
+
+// Q:4 count the occurences of a specific chracter with in a string?
+
+// let str = "My name is Ahsan Tahir";
+// let newstr=str.toLowerCase();
+
+// let charToCount = 'a';
+// let count = 0;
+
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === charToCount) {
+//             count++;
+//         }
+//     }
+
+// console.log("Occurrences of '" + charToCount + "':", count);
+
+
+// // Q:6 write a program to capitalize first letter of each word in a given string
+
+// let inputString = "hello, my name is ahsan";
+
+// // Split the string into an array of words
+// let words = inputString.split(" ");
+// console.log(words);
+
+// for (let i = 0; i < words.length; i++) {
+//     // Capitalize the first letter of each word
+//     words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+// }
+
+
+// let capitalizedString = words.join(" ");
+
+// console.log("Capitalized String:", capitalizedString);
+
+
+
+// // Q:7 remove leading and trailing whitespaces from a string?
+
+// let userName = prompt("Enter your Name:");
+// console.log(userName);
+// let trimName = userName.trim();
+// console.log(trimName);
+
+// // Q:8 Count the number of vowels and constants in a a given string?
+
+// let str = "An Apple a day keep doctor away";
+// str = str.toLowerCase();
+// vowelCount = 0;
+// constantCount = 0;
+
+// for (i = 0; i < str.length; i++) {
+//      currentChar = str[i]
+//     if (currentChar === 'a' || currentChar === 'e' || currentChar === 'i' || currentChar === 'o' || currentChar === 'u') {
+//         vowelCount++;
+//     } else {
+//         constantCount++;
+//     }
+// }
+// console.log(vowelCount);
+// console.log(constantCount);
+
+
+// Q:11 Count the number of words in a given string?
+
+let str = "Hello my name is Ahsan Tahir";
+str = str.split(" ");
+count = 0;
+for(i=0; i<str.length,i++){
+
+}
+console.log(str);
